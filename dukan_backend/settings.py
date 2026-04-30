92ed82cbc23f4498aca1e81a1f58cdc72b3e3ae8
penwang <npenwang@gmail.com>
2026-05-01 00:34:04 +0530

    fix allowed hosts and csrf

--- dukan_backend/settings.py
+++ dukan_backend/settings.py
@@ -23,7 +23,15 @@ if not SECRET_KEY:
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "dukanbackend-production-abc6.up.railway.app",
+    "localhost",
+    "127.0.0.1"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://dukanbackend-production-abc6.up.railway.app"
+]
 
 # ========================
 # APPS
