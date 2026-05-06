ff04083a7dac253071c66aa3e7a56c68d8214867
penwang <npenwang@gmail.com>
2026-05-06 10:33:16 +0530

    fix database url

--- core/models.py
+++ core/models.py
@@ -204,7 +204,7 @@ class OTP(models.Model):
     email = models.EmailField()
     otp = models.CharField(max_length=6)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    is_verified = models.BooleanField(default=False)
     def is_valid(self):
         return timezone.now() < self.created_at + timedelta(minutes=5)
 
