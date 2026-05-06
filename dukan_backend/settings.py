3504ed127e4edeb92512d131f227043913d7d589
penwang <npenwang@gmail.com>
2026-05-06 11:36:25 +0530

    fix database url

--- dukan_backend/settings.py
+++ dukan_backend/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 # ========================
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
