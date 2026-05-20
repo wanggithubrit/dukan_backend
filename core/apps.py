3aee378fd52111784109c92ffe7a27f3e8bff46c
penwang <npenwang@gmail.com>
2026-05-20 16:13:18 +0530

    auto create admin

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,25 @@
 from django.apps import AppConfig
+from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
-    name = "core"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core'
+
+    def ready(self):
+        try:
+            from django.contrib.auth import get_user_model
+
+            User = get_user_model()
+
+            username = "admin"
+
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(
+                    username=username,
+                    email="admin@gmail.com",
+                    password="admin123"
+                )
+
+        except Exception:
+            pass
