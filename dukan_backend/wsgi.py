1d56b7220a83cadbe2fc25b0354853ba35df221b
penwang <npenwang@gmail.com>
2026-06-11 11:29:02 +0530

    Fix WSGI execution order to avoid double setup crash

--- dukan_backend/wsgi.py
+++ dukan_backend/wsgi.py
@@ -8,18 +8,16 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-import django
-from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dukan_backend.settings")
-django.setup()
+
+application = get_wsgi_application()
 
 try:
+    from django.core.management import call_command
     print("Running database migrations on WSGI startup...")
     call_command("migrate", interactive=False)
     print("Database migrations applied successfully!")
 except Exception as e:
     print("Failed to run migrations on startup:", e)
-
-application = get_wsgi_application()
